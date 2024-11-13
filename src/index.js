d566fc4c453b731ab42a10fd6dd9be6164720c13
Go-Groups-Nasah <you@example.com>
2024-11-13 09:05:38 +0100

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'jquery/dist/jquery.min.js';
+import "./components/public/js/app";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
